ddad1f2979a4e086e19c2087f809babb494ded9e
Patrick Ofilada <pcofilada@gmail.com>
2016-03-12 21:32:19 +0800

    Adds routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import StartupList from './containers/startup_list';
+import StartupNew from './containers/startup_new';
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={StartupList} />
+    <Route path="startups/new" component={StartupNew} />
+  </Route>
 );

29e004028e72bced582495303bb02030cdca95dc
Patrick Ofilada <pcofilada@gmail.com>
2016-03-13 23:51:40 +0800

    Adds formReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import StartupsReducer from './reducer_startups';
 
 const rootReducer = combineReducers({
-  startups: StartupsReducer
+  startups: StartupsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
